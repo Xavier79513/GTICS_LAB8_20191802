a9fe6c60e3a6e28bc455ee7232c422f613d2c1ad
Xavier79513 <144184388+Xavier79513@users.noreply.github.com>
2024-10-30 19:37:03 -0500

    inicio

---
+++ lab8_20191802/src/test/java/com/example/lab8_20191802/Lab820191802ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20191802;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820191802ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
